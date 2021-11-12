028be7afe624050500a72937980d5f5b0e750c4f
James Rostirolla <j_rostirolla@hotmail.com>
2021-11-13 10:39:35 +1100

    Constants

---
+++ src/Constants.ts
@@ -0,0 +1,12 @@
+
+export const PADDLE_HEIGHT = 0.2;
+
+export const PADDLE_WIDTH = 0.05;
+
+export const PADDLE_WALLSPACING = 0.02;
+
+export const BALL_WIDTH = 0.01;
+
+export const ASPECT_RATIO = 1080/1920;
+
+export const BALL_HEIGHT = BALL_WIDTH * ASPECT_RATIO;
