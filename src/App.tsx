7349fcf949a03336ec88762325df9af16d9bd995
James Rostirolla <j_rostirolla@hotmail.com>
2021-11-13 11:57:09 +1100

    React elements created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import './App.css';
 import React from "react";
+import { PongRenderer } from './Components/PongRenderer';
+import { IPongRenderGameState } from './IPongRenderGameState';
 
-const App = () => <h1>Hello world</h1>;
+const testState: IPongRenderGameState = {
+    ballX: 0.01,
+    ballY: 0.01,
+    paddleAPosition: 0,
+    paddleBPosition: 0,
+    scoreA: 0,
+    scoreB: 0,
+}
+
+const App = () => <PongRenderer width={1080} gameState={testState}  />;
 
 export default App;
---
+++ src/Components/PongRenderer.tsx
@@ -0,0 +1,67 @@
+import { CSSProperties } from "react";
+import { IPongRenderGameState } from "../IPongRenderGameState";
+
+export interface Props {
+    width: number;
+    gameState: IPongRenderGameState;
+}
+
+const styles: Record<string, CSSProperties> = {
+    pongBackground: {
+        background: "black",
+        width: "100%",
+        height: "100vh"
+    },
+    textStyle: {
+        color: "white",
+        textAlign: "center",
+    },
+    paddleLeft: {
+        width: "10px",
+        height: "100px",
+        background: "white",
+        margin: "0 0 0 20px",
+        position: "absolute",
+        left: 0
+    },
+    centerLine: {
+        width: 0,
+        height: "100vh",
+        border: "white 5px solid",
+        borderStyle: "none dashed none none",
+        left: "49.8%"
+    },
+    paddleRight: {
+        width: "10px",
+        height: "100px",
+        background: "white",
+        margin: "0 20px 0 0",
+        position: "absolute",
+        right: 0
+    }
+}
+
+export const PongRenderer: React.FC<Props> = (props) => {
+    return <div id="MainContainer" style={styles.pongBackground}>
+        <h1 style={styles.textStyle}>PONG</h1>
+            <div>
+                <section id="Score">
+                    <h1 style={styles.textStyle}>0 : 0</h1>
+                </section>
+                <section id="PaddleOne">
+                    <div style={styles.paddleLeft}>
+                    </div>
+                </section>
+
+                <section id="PaddleTwo">
+                    <div style={styles.paddleRight}>
+                    </div>                
+                <section>
+                    <div id="CentreLine" style={styles.centerLine}>
+
+                    </div>
+                </section>
+                </section>
+            </div>
+    </div>
+}
