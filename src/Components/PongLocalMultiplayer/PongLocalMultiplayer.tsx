a462866b4fa33d3495a24145b92e0a9b6a544b4e
Jye Lewis <jye@jyelewis.com>
2021-11-13 15:01:45 +1100

    <PongLocalMultiplayer /> component, for local multiplayer (not yet hooked up to the renderer)

---
+++ src/Components/PongLocalMultiplayer/PongLocalMultiplayer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import { usePongGameController } from "../../hooks/usePongGameController";
+
+export const PongLocalMultiplayer: React.FC = () => {
+  // const pongController = usePongGameController();
+
+  return <h1>Hello world</h1>;
+};
