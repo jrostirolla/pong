02c056bcae814188d51dfc0b2530f19a9bbf331d
Jye Lewis <jye@jyelewis.com>
2021-11-13 15:00:28 +1100

    Fixed bug in constants, added WALL_TO_BALL_WHEN_TOUCHING_PADDLE

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,3 @@
-
 export const PADDLE_HEIGHT = 0.2;
 
 export const PADDLE_WIDTH = 0.02;
@@ -7,11 +6,12 @@ export const PADDLE_WALLSPACING = 0.02;
 
 export const BALL_WIDTH = 0.015;
 
-export const ASPECT_RATIO = 1080/1920;
+export const ASPECT_RATIO = 1080 / 1920;
 
 export const BALL_HEIGHT = BALL_WIDTH / ASPECT_RATIO;
 
 export const PADDLE_MIN = PADDLE_HEIGHT / 2;
 export const PADDLE_MAX = 1 - PADDLE_HEIGHT / 2;
 
-export const WALL_TO_BALL_WHEN_TOUCHING_PADDLE = PADDLE_WALLSPACING + PADDLE_WIDTH + (BALL_WIDTH / 2);
+export const WALL_TO_BALL_WHEN_TOUCHING_PADDLE =
+  PADDLE_WALLSPACING + PADDLE_WIDTH + BALL_WIDTH / 2;
